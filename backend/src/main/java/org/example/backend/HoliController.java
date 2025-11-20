0638a087ddebd2d8386010cf5f28665e7445aef9
nila <20222169@uatx.mx>
2025-11-20 08:58:17 -0600

    Prueba inicial

---
+++ backend/src/main/java/org/example/backend/HoliController.java
@@ -0,0 +1,11 @@
+package org.example.backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HoliController {
+    @GetMapping("/hola")
+    public String decirHola() {
+        return "¡Holi sonia y oscar, ya funciona el servidor Spring Boot 🍃";
+    }
+}
