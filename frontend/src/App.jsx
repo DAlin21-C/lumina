e71b7d68aebd6b397992050229c60c9de2d56ab8
Oscar <oscarahuatzicorona@gmail.com>
2026-01-21 15:16:29 -0600

    interfas codigo y interfas recuperar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./paginas/Login.jsx";
+import FormurarioRecuperación from "./paginas/Recuperar.jsx";
 import CodigoRecuperacion from "./paginas/Codigo.jsx";
+
 
 function App() {
     return (
--- frontend/src/paginas/Recuperar.jsx
+++ frontend/src/paginas/Recuperar.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import '../plantillascss/Codigo.css';
+import '../plantillascss/Recuperar.css';
 
 
 // Rutas de las imágenes estáticas (asumiendo que están en 'public/')
 const LogoCBTA = '/cbta.png';
-const CharacterImage = '/camaleon.png';
+const CharacterImage = '/camaleon_estresado 1.png';
 
-function Codigo() {
+function Recuperar(props) {
     return (
         <div className="login-page">
 
             {/* 1. Sección Izquierda: Ilustración y Círculo */}
             <div className="left-section">
-                <div className="left-circle-bg">
-                    {/* Elemento del camaleón */}
-                    <img
-                        src={CharacterImage}
-                        alt="Personaje Camaleón CBTA 134"
-                        className="character-image"
-                    />
-                </div>
+                <img src="/icono_codigo.png" alt="Logo codigo" className="icon-codigo" />
+                <img
+                    src={CharacterImage}
+                    alt="Personaje Camaleón CBTA 134"
+                    className="character-image"
+                />
+                <button type="submit" className="regresar-button">
+                    Regresar
+                </button>
+
             </div>
 
             {/* 2. Sección Derecha: Formulario de Inicio de Sesión */}
@@ -34,17 +36,22 @@ function Codigo() {
                     <img src={LogoCBTA} alt="CBTA 134 Logo" className="cbta-logo" />
 
                     {/* Textos de bienvenida y sesión */}
-                    <h3 className="session-text">Código de verificación</h3>
+                    <h3 className="session-text">Recuperación de </h3>
+                    <h3 className="session-text">contraseña</h3>
 
                     {/* Formulario */}
                     <form>
                         <div className="input-group">
-                            <label htmlFor="user-input">****</label>
+                            <label htmlFor="user-input">Usuario</label>
                             <input type="text" id="user-input" />
                         </div>
 
+                        <div className="input-group">
+                            <label htmlFor="password-input">Correo electronico</label>
+                            <input type="password" id="password-input" />
+                        </div>
                         <button type="submit" className="login-button">
-                            Siguiente
+                            Enviar
                         </button>
                     </form>
                 </div>
@@ -53,4 +60,4 @@ function Codigo() {
     );
 }
 
-export default Codigo;
+export default Recuperar;
